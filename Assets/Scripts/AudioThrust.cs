bcba2045e3e255c07a74052feac4a5944be8c977
arunlakshmanan <c.arun.lakshmanan@gmail.com>
2016-04-27 18:40:56 -0500

    rename file and cleaner code

--- Assets/Scripts/AudioThrust.cs
+++ Assets/Scripts/AudioThrust.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Globalization;
 
-public class Audio : MonoBehaviour
+public class AudioThrust : MonoBehaviour
 {
     
     public static AudioSource quad_sound; //new audiosource for quad's sounds.
@@ -13,17 +13,9 @@ public class Audio : MonoBehaviour
     public static AudioSource prop_3_sound;
     public static AudioSource prop_4_sound;
 
-    double velocity_squared; //will be summation of x,y,and z velocities
-
-    //public static Kinematics ThrustSource;//receives thrust values from Kinematics script. Both Kinematics and Audio script willbe attached to same gameobject, in this case the quadrotor.
-    //public static PropRotation_New PropAudio;
-   
-
-
     // Use this for initialization
     void Start()
     {
-
         //  Set up audio
         quad_sound = gameObject.AddComponent<AudioSource>();
 
@@ -35,24 +27,20 @@ public class Audio : MonoBehaviour
         AudioClip staticSound; //create an audio clip variable that will take the audioclip file we need for the quad
         staticSound = (AudioClip)Resources.Load("Quadrotor_MidThrottle_firstTake");
 
-        
-
         //attach clip to audio sample
-       quad_sound.clip = staticSound; //the file "heli_500_100%" is now a clip in the AudioSource quad_sound
+        quad_sound.clip = staticSound; 
         prop_1_sound.clip = staticSound;
         prop_2_sound.clip = staticSound;
         prop_3_sound.clip = staticSound;
         prop_4_sound.clip = staticSound;
-
-
 
         //set up sound settings
         quad_sound.loop = true; //enable looping of sound.
         quad_sound.minDistance = 1f;
         quad_sound.maxDistance = 17.8f;
         quad_sound.spatialBlend = 1f;
-       quad_sound.ignoreListenerVolume = true;
-       quad_sound.spread = 194;
+        quad_sound.ignoreListenerVolume = true;
+        quad_sound.spread = 194;
 
         prop_1_sound.loop = true; //enable looping of sound.
         prop_1_sound.minDistance = 1f;
@@ -82,11 +70,6 @@ public class Audio : MonoBehaviour
         prop_4_sound.ignoreListenerVolume = true;
         prop_4_sound.spread = 194;
 
-
-        //quad_sound.mute = true;//INITIALLY SHOULD BE MUTE
-
-        //quad_sound.dopplerLevel = 3;
-      //  quad_sound.Play();
         prop_1_sound.Play();
         prop_2_sound.Play();
         prop_3_sound.Play();
@@ -98,32 +81,15 @@ public class Audio : MonoBehaviour
     void Update()
     {
 
-        //if (gameObject.GetComponent<Kinematics>().thrust_prop1 <= 220000 || gameObject.GetComponent<Kinematics>().thrust_prop1 >= 190000)
-        //{ 
-        //keep reading thrust values 
-        //quad_sound.volume = 0.2f + (float)(0.9 * ThrustSource.thrust_actual);
         prop_1_sound.volume = 0.5f + (float)(0.0000008 * gameObject.GetComponent<Kinematics>().thrust_prop1);
-        //}
-
-
-
         prop_2_sound.volume = 0.5f + (float)(0.0000008 * gameObject.GetComponent<Kinematics>().thrust_prop2);
         prop_3_sound.volume = 0.5f + (float)(0.0000008 * gameObject.GetComponent<Kinematics>().thrust_prop3);
         prop_4_sound.volume = 0.5f + (float)(0.0000008 * gameObject.GetComponent<Kinematics>().thrust_prop4);
-        //  }
 
-
-       // quad_sound.pitch = 0.5f + (float)(0.6 * Mathf.Sqrt(System.Math.Abs((float)ThrustSource.thrust_actual)));
-        prop_1_sound.pitch = 0.2f +(float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop1)));
-        Debug.Log(prop_1_sound.pitch);
+        prop_1_sound.pitch = 0.2f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop1)));
         prop_2_sound.pitch = 0.2f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop2)));
         prop_3_sound.pitch = 0.2f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop3)));
         prop_4_sound.pitch = 0.2f + (float)(0.001* Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop4)));
-
-        //
-        //Debug.Log(quad_sound.pitch);
-
-
 
         //Conditional statements on boundaries of sound and visibility.
         if (gameObject.transform.position.x >= 55 || gameObject.transform.position.x <= -11 || gameObject.transform.position.z >= 58.4 || gameObject.transform.position.z <= 39)
@@ -138,17 +104,11 @@ public class Audio : MonoBehaviour
 
         else
         {
-            //quad_sound.mute = false;
             prop_1_sound.mute = false;
             prop_2_sound.mute = false;
             prop_3_sound.mute = false;
             prop_4_sound.mute = false;
             gameObject.transform.localScale = new Vector3(1f,1f,1f);
         }
-
-
-
-
-
     }
 }
