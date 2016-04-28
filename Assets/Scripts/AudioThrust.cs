a4afe37f68d5af33798c33164a3c60d1d7cb292a
arunlakshmanan <c.arun.lakshmanan@gmail.com>
2016-04-27 21:59:59 -0500

    some more fixes

--- Assets/Scripts/AudioThrust.cs
+++ Assets/Scripts/AudioThrust.cs
@@ -6,8 +6,6 @@ using System.Globalization;
 
 public class AudioThrust : MonoBehaviour
 {
-    
-    public static AudioSource quad_sound; //new audiosource for quad's sounds.
     public static AudioSource prop_1_sound;
     public static AudioSource prop_2_sound;
     public static AudioSource prop_3_sound;
@@ -17,30 +15,19 @@ public class AudioThrust : MonoBehaviour
     void Start()
     {
         //  Set up audio
-        quad_sound = gameObject.AddComponent<AudioSource>();
-
         prop_1_sound = GameObject.Find("Prop1").AddComponent<AudioSource>();
         prop_2_sound = GameObject.Find("Prop2").AddComponent<AudioSource>();
         prop_3_sound = GameObject.Find("Prop3").AddComponent<AudioSource>();
         prop_4_sound = GameObject.Find("Prop4").AddComponent<AudioSource>();
 
         AudioClip staticSound; //create an audio clip variable that will take the audioclip file we need for the quad
-        staticSound = (AudioClip)Resources.Load("Quadrotor_MidThrottle_firstTake");
+		staticSound = (AudioClip)Resources.Load("Quadrotor_MidThrottle_firstTake");
 
         //attach clip to audio sample
-        quad_sound.clip = staticSound; 
         prop_1_sound.clip = staticSound;
         prop_2_sound.clip = staticSound;
         prop_3_sound.clip = staticSound;
         prop_4_sound.clip = staticSound;
-
-        //set up sound settings
-        quad_sound.loop = true; //enable looping of sound.
-        quad_sound.minDistance = 1f;
-        quad_sound.maxDistance = 17.8f;
-        quad_sound.spatialBlend = 1f;
-        quad_sound.ignoreListenerVolume = true;
-        quad_sound.spread = 194;
 
         prop_1_sound.loop = true; //enable looping of sound.
         prop_1_sound.minDistance = 1f;
@@ -86,26 +73,26 @@ public class AudioThrust : MonoBehaviour
         prop_3_sound.volume = 0.5f + (float)(0.0000008 * gameObject.GetComponent<Kinematics>().thrust_prop3);
         prop_4_sound.volume = 0.5f + (float)(0.0000008 * gameObject.GetComponent<Kinematics>().thrust_prop4);
 
-        prop_1_sound.pitch = 0.2f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop1)));
-        prop_2_sound.pitch = 0.2f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop2)));
-        prop_3_sound.pitch = 0.2f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop3)));
-        prop_4_sound.pitch = 0.2f + (float)(0.001* Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop4)));
+        prop_1_sound.pitch = 0.8f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop1)));
+        prop_2_sound.pitch = 0.8f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop2)));
+        prop_3_sound.pitch = 0.8f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop3)));
+        prop_4_sound.pitch = 0.8f + (float)(0.001* Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop4)));
 
         //Conditional statements on boundaries of sound and visibility.
-        if (gameObject.transform.position.x >= 55 || gameObject.transform.position.x <= -11 || gameObject.transform.position.z >= 58.4 || gameObject.transform.position.z <= 39)
-        {
-            prop_1_sound.mute = true;
-            prop_2_sound.mute = true;
-            prop_3_sound.mute = true;
-            prop_4_sound.mute = true;
-        }
-
-        else
-        {
-            prop_1_sound.mute = false;
-            prop_2_sound.mute = false;
-            prop_3_sound.mute = false;
-            prop_4_sound.mute = false;
-        }
+//        if (gameObject.transform.position.x >= 55 || gameObject.transform.position.x <= -11 || gameObject.transform.position.z >= 58.4 || gameObject.transform.position.z <= 39)
+//        {
+//            prop_1_sound.mute = true;
+//            prop_2_sound.mute = true;
+//            prop_3_sound.mute = true;
+//            prop_4_sound.mute = true;
+//        }
+//
+//        else
+//        {
+//            prop_1_sound.mute = false;
+//            prop_2_sound.mute = false;
+//            prop_3_sound.mute = false;
+//            prop_4_sound.mute = false;
+//        }
     }
 }
