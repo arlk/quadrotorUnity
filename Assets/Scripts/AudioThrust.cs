abf8bd3509625aeba1c6a35102b881beae1c6536
arunlakshmanan <c.arun.lakshmanan@gmail.com>
2016-04-28 00:38:57 -0500

    audio script simpler

--- Assets/Scripts/AudioThrust.cs
+++ Assets/Scripts/AudioThrust.cs
@@ -10,60 +10,21 @@ public class AudioThrust : MonoBehaviour
     public static AudioSource prop_2_sound;
     public static AudioSource prop_3_sound;
     public static AudioSource prop_4_sound;
-	float distanceToCamera;
+
+	public float soundBubble;
+
+	GameObject PlayerCamera;
 
     // Use this for initialization
     void Start()
     {
         //  Set up audio
-        prop_1_sound = GameObject.Find("Prop1").AddComponent<AudioSource>();
-        prop_2_sound = GameObject.Find("Prop2").AddComponent<AudioSource>();
-        prop_3_sound = GameObject.Find("Prop3").AddComponent<AudioSource>();
-        prop_4_sound = GameObject.Find("Prop4").AddComponent<AudioSource>();
+		prop_1_sound = GameObject.Find("Prop1").GetComponent<AudioSource>();
+        prop_2_sound = GameObject.Find("Prop2").GetComponent<AudioSource>();
+        prop_3_sound = GameObject.Find("Prop3").GetComponent<AudioSource>();
+        prop_4_sound = GameObject.Find("Prop4").GetComponent<AudioSource>();
 
-        AudioClip staticSound; //create an audio clip variable that will take the audioclip file we need for the quad
-		staticSound = (AudioClip)Resources.Load("Quadrotor_MidThrottle_firstTake");
-
-        //attach clip to audio sample
-        prop_1_sound.clip = staticSound;
-        prop_2_sound.clip = staticSound;
-        prop_3_sound.clip = staticSound;
-        prop_4_sound.clip = staticSound;
-
-        prop_1_sound.loop = true; //enable looping of sound.
-        prop_1_sound.minDistance = 1f;
-        prop_1_sound.maxDistance = 17.8f;
-        prop_1_sound.spatialBlend = 1f;
-        prop_1_sound.ignoreListenerVolume = true;
-        prop_1_sound.spread = 194;
-
-        prop_2_sound.loop = true; //enable looping of sound.
-        prop_2_sound.minDistance = 1f;
-        prop_2_sound.maxDistance = 17.8f;
-        prop_2_sound.spatialBlend = 1f;
-        prop_2_sound.ignoreListenerVolume = true;
-        prop_2_sound.spread = 194;
-
-        prop_3_sound.loop = true; //enable looping of sound.
-        prop_3_sound.minDistance = 1f;
-        prop_3_sound.maxDistance = 17.8f;
-        prop_3_sound.spatialBlend = 1f;
-        prop_3_sound.ignoreListenerVolume = true;
-        prop_3_sound.spread = 194;
-
-        prop_4_sound.loop = true; //enable looping of sound.
-        prop_4_sound.minDistance = 1f;
-        prop_4_sound.maxDistance = 17.8f;
-        prop_4_sound.spatialBlend = 1f;
-        prop_4_sound.ignoreListenerVolume = true;
-        prop_4_sound.spread = 194;
-
-        prop_1_sound.Play();
-        prop_2_sound.Play();
-        prop_3_sound.Play();
-        prop_4_sound.Play();
-
-		distanceToCamera = 0.0f;
+		PlayerCamera = GameObject.Find("Main Camera");
 
     }
 
@@ -81,22 +42,5 @@ public class AudioThrust : MonoBehaviour
         prop_3_sound.pitch = 0.8f + (float)(0.001 * Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop3)));
         prop_4_sound.pitch = 0.8f + (float)(0.001* Mathf.Sqrt(System.Math.Abs((float)gameObject.GetComponent<Kinematics>().thrust_prop4)));
 
-		distanceToCamera = 
-        //Conditional statements on boundaries of sound and visibility.
-//        if (gameObject.transform.position.x >= 55 || gameObject.transform.position.x <= -11 || gameObject.transform.position.z >= 58.4 || gameObject.transform.position.z <= 39)
-//        {
-//            prop_1_sound.mute = true;
-//            prop_2_sound.mute = true;
-//            prop_3_sound.mute = true;
-//            prop_4_sound.mute = true;
-//        }
-//
-//        else
-//        {
-//            prop_1_sound.mute = false;
-//            prop_2_sound.mute = false;
-//            prop_3_sound.mute = false;
-//            prop_4_sound.mute = false;
-//        }
     }
 }
